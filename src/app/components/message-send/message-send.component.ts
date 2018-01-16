4a0c0c463a007e276a7b96d21f42f4e333c61407
Kots Olesya <kots.olesya@gmail.com>
2018-01-16 23:05:03 +0200

    message-send

--- src/app/components/message-send/message-send.component.ts
+++ src/app/components/message-send/message-send.component.ts
@@ -1,4 +1,6 @@
-import { Component,Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { MessagesService } from '../../rest/messages.service';
+import { MessageDto } from './../../rest/message.dto';
 
 @Component({
     selector: 'app-message-send',
@@ -6,5 +8,21 @@ import { Component,Input, Output, EventEmitter, OnInit } from '@angular/core';
 })
 
 export class MessageSendComponent {
-   
+    @Output() sent = new EventEmitter<never>();
+    @Input() userId: number;
+
+    textMessage: string;
+
+    constructor(private messagesService: MessagesService) { }
+
+    send() {
+        const message = new MessageDto();
+        message.date = new Date(Date.now());
+        message.text = this.textMessage;
+        message.userId = this.userId;
+        this.textMessage = '';
+        this.messagesService.add(message).subscribe(() => {
+            this.sent.emit();
+        });
+    }
 }
